0c6fdbce70c1faa452aa446dbc0ad86d8e834900
Paul <pfultz2@yahoo.com>
2017-02-26 03:56:51 -0600

    Improve header cmake

--- cget/cmake/header.cmake
+++ cget/cmake/header.cmake
@@ -1,13 +1,38 @@
 cmake_minimum_required (VERSION 2.8)
 
-set(INCLUDE_DIR include CACHE PATH "Include directory")
+set(INCLUDE_DIR include CACHE STRING "Include directory")
+message(STATUS "Include directory: ${INCLUDE_DIR}")
+set(_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/${INCLUDE_DIR})
+set(HEADER_DIR ${_INCLUDE_DIR} CACHE STRING "Directory of headers to be installed")
+set(_HEADER_DIR ${CMAKE_CURRENT_SOURCE_DIR}/${HEADER_DIR})
 
-install(DIRECTORY ${INCLUDE_DIR}/ DESTINATION include
-    FILES_MATCHING 
-    PATTERN "*.h"
-    PATTERN "*.hpp"
-    PATTERN "*.hh"
-    PATTERN "*.hxx"
-)
+if(NOT EXISTS ${_INCLUDE_DIR})
+    if(EXISTS ${_HEADER_DIR})
+        install(DIRECTORY ${_HEADER_DIR} DESTINATION include
+            FILES_MATCHING 
+            PATTERN "*.h"
+            PATTERN "*.hpp"
+            PATTERN "*.hh"
+            PATTERN "*.hxx"
+        )
+    else()
 
+        file(GLOB HEADER_FILES 
+            *.h
+            *.hpp
+            *.hh
+            *.hxx)
+        install(FILES ${HEADER_FILES} DESTINATION include)
+    endif()
+
+else()
+    install(DIRECTORY ${_INCLUDE_DIR}/ DESTINATION include
+        FILES_MATCHING 
+        PATTERN "*.h"
+        PATTERN "*.hpp"
+        PATTERN "*.hh"
+        PATTERN "*.hxx"
+    )
+
+endif()
 include(CTest)
